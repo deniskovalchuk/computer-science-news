54013bae7ce3dce27857e375584b1a225af300f0
deniskovalchuk <deniskk25@gmail.com>
2016-12-15 21:20:20 +0300

    Logic.

--- app/src/main/java/cs/petrsu/ru/imitnews/news/News.java
+++ app/src/main/java/cs/petrsu/ru/imitnews/news/News.java
@@ -1,56 +1,30 @@
 package cs.petrsu.ru.imitnews.news;
 
-import android.content.Context;
-import android.os.Build;
-import android.text.Html;
-
-import org.jsoup.nodes.Element;
-
-import cs.petrsu.ru.imitnews.R;
-
 /**
  * Created by Kovalchuk Denis on 22.11.16.
  * Email: deniskk25@gmail.com
  */
 
 public class News {
-    private Context context;
+    private String html;
     private String title;
     private String content;
-    private String html;
 
-    public News(Context context, Element html) {
-        this.context = context;
-        this.html = html.toString();
-        setTitle(html);
-        setContent();
+    public News(String html, String title, String content) {
+        this.html = html;
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getHtml() {
+        return html;
     }
 
     public String getTitle() {
         return title;
     }
 
-    private void setTitle(Element html) {
-        String date = html.select("b").text();
-        title = html.select("span.title").text();
-        if (title.isEmpty()) {
-            title = context.getString(R.string.default_title) + date;
-        }
-    }
-
     public String getContent() {
         return content;
-    }
-
-    private void setContent() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            content = Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY).toString();
-        } else {
-            content = Html.fromHtml(html).toString();
-        }
-    }
-
-    public String getHtml() {
-        return html;
     }
 }
--- app/src/main/java/cs/petrsu/ru/imitnews/parser/NewsParser.java
+++ app/src/main/java/cs/petrsu/ru/imitnews/parser/NewsParser.java
@@ -1,6 +1,8 @@
 package cs.petrsu.ru.imitnews.parser;
 
 import android.content.Context;
+import android.os.Build;
+import android.text.Html;
 
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -9,6 +11,7 @@ import org.jsoup.select.Elements;
 import java.util.ArrayList;
 import java.util.List;
 
+import cs.petrsu.ru.imitnews.R;
 import cs.petrsu.ru.imitnews.news.News;
 
 /**
@@ -18,18 +21,30 @@ import cs.petrsu.ru.imitnews.news.News;
 
 public class NewsParser {
     private static final String TAG = "NewsParser";
+    private static Element newsHtml;
 
-    public static List<News> createListNews(Context context, Document html) {
+    public static List<News> createListNews(Context context, Document document) {
         List<News> newsList = new ArrayList<>();
-        Elements allNews = html.select("div[id]");
-
+        Elements allNews = document.select("div[id]");
         for (Element currentNews : allNews) {
-            Element newsHtml = currentNews.select("div").get(0);
-            News news = new News(context, newsHtml);
+            newsHtml = currentNews.select("div").get(0);
+            News news = new News(newsHtml.toString(), parseTitle(context), parseContent());
             newsList.add(news);
         }
-
         return newsList;
     }
-}
 
+    private static String parseTitle(Context context) {
+        String title = newsHtml.select("span.title").text();
+        String date = newsHtml.select("b").text();
+        return title.isEmpty() ? context.getString(R.string.default_title) + date : title;
+    }
+
+    private static String parseContent() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return Html.fromHtml(newsHtml.toString(), Html.FROM_HTML_MODE_LEGACY).toString();
+        } else {
+            return Html.fromHtml(newsHtml.toString()).toString();
+        }
+    }
+}
